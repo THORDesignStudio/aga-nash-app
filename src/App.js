d7ec45f7273b6d76a78ab8222d7a45431ece878c
Yousef Ahmad <yousef.m.ahmad@outlook.com>
2021-03-31 19:00:37 -0400

    :lipstick: Trying to get styled components working. The simulator is running the button, but not applying the styling.

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { Text, View, StyleSheet } from "react-native";
+import Button from "./components/primitives/button";
+
+export default function App() {
+  return (
+    <View style={styles.container} >
+      <Text>Open up App.js to start working on your app!</Text>
+      <Button title="Hello World" bgColor={'red'}/>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+});
