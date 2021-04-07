30fb61e56e20a86b1b4e2f53a47f6a09dbabc9cb
Yousef Ahmad <yousef.m.ahmad@outlook.com>
2021-04-06 23:32:50 -0400

    Removed the primitives folder along with the primitive button. This should be a stable master branch now.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { Text, View, StyleSheet } from "react-native";
-import Button from "./components/primitives/button";
 
 export default function App() {
   return (
     <View style={styles.container} >
       <Text>Open up App.js to start working on your app!</Text>
-      <Button title="Hello World" bgColor={'red'}/>
       <StatusBar style="auto" />
     </View>
   );
