2789c347651408f395d17ca5d922dde240db2b61
Yousef Ahmad <yousef.m.ahmad@outlook.com>
2021-04-12 23:40:50 -0400

    Updated intro page to match v4 wireframes. Applied the background.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,51 @@
 import React from "react";
-import { Text, View, StyleSheet, ImageBackground } from "react-native";
+import {
+  Text,
+  View,
+  StyleSheet,
+  ImageBackground,
+  TouchableOpacity,
+} from "react-native";
 import Footer from "./components/global/footer";
-import BeginIcon from "./assets/images/BeginIcon";
 
 export default function App() {
   return (
     <>
       <View style={styles.container}>
-        <View style={styles.box}>
-          <View style={styles.borderBottom}>
-            <Text style={styles.nash}>NASH</Text>
+        <ImageBackground
+          source={require("./assets/images/NASH_bkgd.png")}
+          style={styles.background}
+        >
+          <View style={styles.box}>
+            <View>
+              <Text style={styles.nash}>NASH</Text>
+            </View>
+            <View style={styles.criticalCareContainer}>
+              <Text style={styles.criticalCareText}>
+                CRITICAL CARE PATHWAY
+              </Text>
+            </View>
+            <Text style={styles.boldText}>
+              This app helps primary care physicians, endrinologists and
+              gastroenterologists:
+            </Text>
+            <Text style={styles.splashText}>
+              Screen for NAFLD with advanced fibrosis
+            </Text>
+            <Text style={styles.splashText}>
+              Manage patients at risk for NAFL/NASH
+            </Text>
+            <Text style={styles.splashText}>
+              Know when to refer to a hepaologist
+            </Text>
           </View>
-          <Text style={styles.goldText}>
-            This app helps primary care physicians, endrinologists and
-            gastroenterologists
-          </Text>
-          <Text style={styles.splashText}>
-            screen for NAFLD with advanced fibrosis, manage patients at risk for
-            NAFLD/NASH, know when to refer to a hepatologist
-          </Text>
-        </View>
-        <View style={styles.begin}>
-          <BeginIcon />
-          <Text style={styles.beginText}>BEGIN</Text>
-        </View>
+          
+          <View style={styles.riskContainer}>
+            <TouchableOpacity style={styles.risk}>
+              <Text style={styles.riskText}>CALCULATE RISK</Text>
+            </TouchableOpacity>
+          </View>
+        </ImageBackground>
       </View>
       <Footer />
     </>
@@ -33,16 +55,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#faa61a",
     alignItems: "center",
     justifyContent: "center",
   },
   box: {
     flex: 0.45,
     backgroundColor: "rgba(255,255,255,0.8)",
-    marginEnd: 50,
-    marginStart: 50,
-    paddingBottom: 30,
+    paddingBottom: 150,
+    marginHorizontal: 50,
   },
   nash: {
     fontSize: 64,
@@ -53,36 +73,60 @@ const styles = StyleSheet.create({
     fontWeight: "700",
     textAlign: "center",
   },
-  borderBottom: {
+  criticalCareContainer: {
     borderBottomColor: "black",
+    borderTopWidth: 1,
     borderBottomWidth: 1,
     marginEnd: 20,
     marginStart: 20,
   },
-  goldText: {
-    color: "#faa61a",
+  boldText: {
     textAlign: "center",
-    fontSize: 16,
-    fontWeight: "500",
-    marginTop: 25,
+    fontSize: 14,
+    fontWeight: "700",
+    marginVertical: 10,
     marginEnd: 10,
     marginStart: 10,
   },
   splashText: {
     textAlign: "center",
-    fontSize: 16,
-    marginEnd: 10,
-    marginStart: 10,
+    fontSize: 14,
+    marginHorizontal: 15,
+    marginVertical: 10,
     fontWeight: "500",
   },
-  begin: {
-    marginTop: 20,
-    alignContent: "center",
+  riskContainer:{
+    alignItems: "center",
+    justifyContent: "center",
   },
-  beginText: {
+  risk: {
+    marginTop: 40,
+    backgroundColor: 'white',
+    width: '60%',
+    borderStyle: 'solid',
+    borderColor: 'black',
+    borderWidth: 1,
+    borderRadius: 20,
+  },
+  riskText: {
     fontSize: 20,
-    color: "white",
+    fontWeight: '700',
+    color: "#faa61a",
     textAlign: "center",
-    marginTop: -10,
+    paddingTop: 30,
+    paddingBottom: 30,
   },
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    width: '100%',
+  },
+  criticalCareText:{
+    textAlign: "center",
+    fontSize: 16,
+    fontWeight: '700',
+    marginVertical: 10,
+    color:"#faa61a",
+  }
 });
