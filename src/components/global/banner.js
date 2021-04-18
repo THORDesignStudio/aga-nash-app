15061cf945b888c363556827ae6c2655dfb26ff5
Yousef Ahmad <yousef.m.ahmad@outlook.com>
2021-04-18 17:06:02 -0400

    :building_construction: Broke out the aga banner from the footer since i realized it wasn't in every screen. :heavy_plus_sign: Added react native navigation.

---
+++ src/components/global/banner.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { View, Image, StyleSheet} from "react-native";
+import banner from "../../assets/images/banner.png";
+
+
+export default function Banner() {
+  return (
+    <>
+      <View style={styles.banner}>
+        <Image source={banner} style={styles.img} />
+      </View>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  banner: {
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+    height: 100,
+  }
+});
