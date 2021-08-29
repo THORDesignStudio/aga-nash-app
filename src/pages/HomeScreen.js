3df747e31ed3ac7df59199ab6ef7ff8ec0f8f5cc
John Serrao <bigbadserrao@gmail.com>
2021-08-29 18:49:53 -0500

    HomeScreen refactored to just use regular styles

--- src/pages/HomeScreen.js
+++ src/pages/HomeScreen.js
@@ -1,34 +1,19 @@
-import React,{useContext, useEffect} from "react";
-import {
-  Text,
-  View,
-  StyleSheet,
-  ImageBackground,
-  PixelRatio,
-} from "react-native";
+import React, { useContext, useEffect } from "react";
+import { Text, View, StyleSheet, ImageBackground } from "react-native";
+import { useFonts } from 'expo-font'
+
+import { ApplicationContext } from "../applicationProvider/applicationProvider";
+
 import Footer from "../components/global/footer";
 import Banner from "../components/global/banner";
 import Button from "../components/primitives/button";
 import Container from "../components/primitives/container";
-import styled from "styled-components/native";
-import {useFonts} from 'expo-font'
-import { ApplicationContext } from "../applicationProvider/applicationProvider";
 
 /**
  * Homescreen
  * 
  * This is the first page that users see. This takes them to the @param {page} Navigate page. 
- * Other than that, note there's some mixture of stylesheet and styled components here. That's because I haven't
- * gotten around to converting things yet.
  */
-
-const Header = styled.Text`
-  fontSize: 25;
-  marginHorizontal: 20px;
-  fontWeight: 700;
-  textAlign: center;
-  fontFamily: 'NunitoSans-Bold';
-`;
 
 export default function HomeScreen({ navigation }) {
   const {setPageId} = useContext(ApplicationContext)
@@ -52,9 +37,9 @@ export default function HomeScreen({ navigation }) {
           style={styles.background}
         >
           <View style={styles.box}>
-            <View>
-              <Header>NONALCOHOLIC FATTY LIVER DISEASE (NASH)</Header>
-            </View>
+            <Text style={styles.header}>
+              NONALCOHOLIC FATTY LIVER DISEASE (NASH)
+            </Text>
             <View style={styles.criticalCareContainer}>
               <Text style={styles.criticalCareText}>
                 Clinical Care Pathway for Risk Stratification and Patient Management
@@ -81,6 +66,12 @@ export default function HomeScreen({ navigation }) {
 }
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+    width: "100%",
+  },
   box: {
     flex: 0.75,
     backgroundColor: "rgba(255,255,255,0.8)",
@@ -93,29 +84,30 @@ const styles = StyleSheet.create({
     borderBottomWidth: 1,
     marginHorizontal: 20,
   },
-  splashText: {
-    textAlign: "center",
-    fontSize: 18,
-    marginHorizontal: 15,
-    fontWeight: "500",
-    fontFamily: 'NunitoSans-Regular',
-  },
-  riskContainer: {
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  background: {
-    flex: 1,
-    resizeMode: "cover",
-    justifyContent: "center",
-    width: "100%",
-  },
   criticalCareText: {
     textAlign: "center",
     fontSize: 16,
     fontWeight: "700",
     marginVertical: 10,
     color: "#faa61a",
+    fontFamily: 'NunitoSans-Regular',
+  },
+  header: {
+    fontSize: 25,
+    marginHorizontal: 20,
+    fontWeight: "700",
+    textAlign: "center",
+    fontFamily: 'NunitoSans-Bold'
+  },
+  riskContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  splashText: {
+    textAlign: "center",
+    fontSize: 18,
+    marginHorizontal: 15,
+    fontWeight: "500",
     fontFamily: 'NunitoSans-Regular',
   },
 });
