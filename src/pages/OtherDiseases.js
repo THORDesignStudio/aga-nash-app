d259cbf290411c1f511d2b2806a5b3498ebf1fee
John Serrao <bigbadserrao@gmail.com>
2021-09-01 03:49:42 -0500

    Other disease check no longer has duplicative components

--- src/pages/OtherDiseases.js
+++ src/pages/OtherDiseases.js
@@ -4,20 +4,11 @@ import Footer from "../components/global/footer";
 import NextBack from "../components/global/nextBackButtons";
 import AbbreviationsFootnotes from "../components/global/abbreviationsFootnotes";
 import Radio from "../components/primitives/radio";
-import PageContainer from '../components/primitives/container'
+import Container from '../components/primitives/container'
 import styled from 'styled-components/native'
 import TextBasic from "../components/global/textBasic";
 import {ApplicationContext} from '../applicationProvider/applicationProvider'
 import TitleContainer from '../components/global/titleContainer'
-
-const ContentTextContainer = styled.View`
-  marginHorizontal: 20px;
-  marginVertical: 10px;
-`
-const ImportantTextContainer = styled.Text`
-  marginHorizontal: 20px;
-  marginVertical: 10px;
-`
 
 export default function OtherForms({ navigation }) {
   const [nextPage, setNextPage] = useState('RiskManagement')
@@ -44,39 +35,56 @@ export default function OtherForms({ navigation }) {
 
   return (
     <>
-      <PageContainer alignItems='flex-start' justifyContent='flex-start'>
+      <Container 
+        alignItems='flex-start' 
+        justifyContent='flex-start'
+      >
         <TitleContainer>
         <PageTitle
           pageTitle="Evaluate for other forms of liver disease:"
         />
         </TitleContainer>
-        <ContentTextContainer>
-        <TextBasic fontSize={18}>
+        <TextBasic 
+          fontSize={18}
+          marginVertical={5}
+          marginHorizontal={20}
+          textAlign='left'
+        >
           Alcohol intake history (≥ 14 drinks/week for women or ≥ 21 drinks/week for men)
         </TextBasic>
-        </ContentTextContainer>
-        <ContentTextContainer>
-        <TextBasic fontSize={18}>
+        <TextBasic 
+          fontSize={18}
+          marginVertical={5}
+          marginHorizontal={20}
+          textAlign='left'
+        >
           HCV antibody with reflex testing of HCV RNA
         </TextBasic>
-        </ContentTextContainer>
-        <ContentTextContainer>
-        <TextBasic fontSize={18}>
+        <TextBasic
+          fontSize={18}
+          marginVertical={5}
+          marginHorizontal={20}   
+          textAlign='left'     
+        >
           Consider ANA, AMA, ASMA, immunoglobulins, ferritin, A1AT 
         </TextBasic>
-        </ContentTextContainer>
-        <ImportantTextContainer>
-        <TextBasic fontSize={18} fontWeight='bold' fontColor='#FC1B1F'>
+        <TextBasic 
+          fontSize={18} 
+          fontWeight='bold' 
+          fontColor='#FC1B1F'
+          marginVertical={25}
+          marginHorizontal={20}  
+          textAlign='left'
+        >
           *If there are other forms of liver disease, click on the button below and then hit 'Next'. Otherwise, simply click 'Next'.
         </TextBasic>
-        </ImportantTextContainer>
         <Radio 
           label='Other forms of liver disease present' 
-          marginHorizontal={30} 
+          marginHorizontal={20} 
           onPressSelected={handleNextPresentSelected} 
           onPressUnselected={handleNextPresentUnselected}
         />
-      </PageContainer>
+      </Container>
       <NextBack 
         navigation={navigation} 
         marginVertical={10} 
