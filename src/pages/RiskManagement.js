a2122483fdcb2046aae94e7a095ab61ec583572f
John Serrao <bigbadserrao@gmail.com>
2021-09-07 14:18:00 -0500

    RiskManagement page logic updated

--- src/pages/RiskManagement.js
+++ src/pages/RiskManagement.js
@@ -1,48 +1,45 @@
-import React, {useContext, useState, useEffect} from "react";
+import React, { useContext, useState, useEffect } from "react";
 import Footer from "../components/global/footer";
 import BackNext from "../components/global/nextBackButtons";
 import AbbreviationsFootnotes from "../components/global/abbreviationsFootnotes";
 import ScrollableView from "../components/primitives/scrollableContainer";
-import LowRiskMng from '../components/content/riskManagement/LowRisk'
+import LowRiskMng from "../components/content/riskManagement/LowRisk";
 import IndeterminateRiskManagement from "../components/content/riskManagement/IndeterminateRisk";
-import HighRiskManagement from '../components/content/riskManagement/HighRisk'
-import { ApplicationContext } from '../applicationProvider/applicationProvider'
+import HighRiskManagement from "../components/content/riskManagement/HighRisk";
+import { ApplicationContext } from "../applicationProvider/applicationProvider";
 
 export default function RiskManagement({ navigation }) {
   const { fib4, setPageId, age } = useContext(ApplicationContext);
-  const [ riskMng, setRiskMng] = useState(<></>)
+  const [riskMng, setRiskMng] = useState(<></>);
 
   useEffect(() => {
     navigation.addListener("focus", () => {
-      if (fib4 < 1.3 && age < 65 || fib4 < 2 && age >= 65) {
+      if (fib4 < 1.3 || (fib4 < 2 && age >= 65)) {
         setPageId("LowRiskMng");
-        setRiskMng(<LowRiskMng />)
-      } else if(age < 65 && 1.3 < fib4 < 2.67 || age > 65 && 2 < fib4 < 2.67 ) {
-        setPageId("IndeterminateRiskMng");
-        setRiskMng (<IndeterminateRiskManagement />);
+        setRiskMng(<LowRiskMng />);
       } else if (fib4 > 2.67) {
         setPageId("HighRiskMng");
         setRiskMng(<HighRiskManagement />);
+      } else if (1.3 < fib4 < 2.67 || (age >= 65 && 2 < fib4 < 2.67)) {
+        setPageId("IndeterminateRiskMng");
+        setRiskMng(<IndeterminateRiskManagement />);
       }
     });
   }, [navigation, fib4]);
-  
 
 
   return (
     <>
-      <ScrollableView>
-        {riskMng}
-      </ScrollableView>
-      <BackNext 
-        navigation={navigation} 
+      <ScrollableView>{riskMng}</ScrollableView>
+      <BackNext
+        navigation={navigation}
         marginVertical="0px"
         paddingVertical="20px"
-        nextPage='Navigate'
+        nextPage="Navigate"
         backgroundColor="#F1F6F8"
       />
-      <AbbreviationsFootnotes navigation={navigation}/>
-      <Footer navigation={navigation}/>
+      <AbbreviationsFootnotes navigation={navigation} />
+      <Footer navigation={navigation} />
     </>
   );
 }
