3489cb896ac05c5418d23485bf5d63b800261140
John Serrao <bigbadserrao@gmail.com>
2021-09-07 13:46:43 -0500

    Content components fixed up

--- src/components/content/results/IndeterminateRisk.js
+++ src/components/content/results/IndeterminateRisk.js
@@ -2,48 +2,44 @@ import React, {useContext} from "react";
 import styled from "styled-components/native";
 import IndeterminateRisk from '../../../assets/images/indeterminateRisk'
 import {ApplicationContext} from '../../../applicationProvider/applicationProvider'
+import TextBasic from "../../global/textBasic";
 
-const FibText = styled.Text`
-  fontFamily: "NunitoSans-Regular";
-  fontSize: 20px;
-`;
-const AltText = styled.Text`
-  fontFamily: "NunitoSans-Regular";
-  fontSize: 20px;
-  marginVertical: 10px;
-`;
 const IndeterminateRiskDial = styled.View`
   marginVertical: 40px;
 `;
 
-const YellowText = styled.Text`
-  fontFamily: "NunitoSans-Regular";
-  fontSize: 20px;
-  color: #E49917;
-`;
-
-const GreaterFourtyText = styled.Text`
-  fontFamily: "NunitoSans-Regular";
-  fontSize: 16px;
-`
-
 export default function IndeterminateRiskContent({ navigation }) {
-const {alt, fib4} = useContext(ApplicationContext);
-
-
+  const {alt, fib4} = useContext(ApplicationContext);
 
   return (
     <>
-        <FibText>
-        FIB4 = {fib4.toFixed(2)}
-        </FibText>
-        {alt >= 40 && <AltText>ALT ≥ 40 U/L</AltText>}
-        <IndeterminateRiskDial>
-          <IndeterminateRisk />
-        </IndeterminateRiskDial>
-        <FibText>
-          <YellowText>INDETERMINATE RISK</YellowText>
-        </FibText>
+      <TextBasic fontSize={24}>FIB4 = {fib4.toFixed(2)}</TextBasic>
+      {alt >= 40 && (
+        <>
+          <TextBasic fontSize={24} marginVertical={5}>
+            {`ALT \u2265 40 U/L`}
+          </TextBasic>
+          <TextBasic fontSize={14}>
+            Risk of other forms of liver disease
+          </TextBasic>
+        </>
+      )}
+      {alt < 40 && (
+        <TextBasic fontSize={24} marginVertical={5}>
+          {`ALT \u003c 40 U/L`}
+        </TextBasic>
+      )}
+      <IndeterminateRiskDial>
+        <IndeterminateRisk />
+      </IndeterminateRiskDial>
+      <TextBasic
+        fontSize={20}
+        marginHorizontal={50}
+        fontColor="#E49917"
+        fontWeight="bold"
+      >
+        INDETERMINATE RISK
+      </TextBasic>
     </>
   );
 }
--- src/components/content/results/LowRisk.js
+++ src/components/content/results/LowRisk.js
@@ -9,55 +9,34 @@ const LowRiskDial = styled.View`
   marginVertical: 40px;
 `;
 
-const GreenText = styled.Text`
-  fontSize: 20px;
-  color: #77c83f;
-`;
-
-const GreaterFourtyText = styled.Text`
-  fontFamily: "NunitoSans-Regular";
-  fontSize: 16px;
-`
-
-export default function LowRiskContent({ navigation }) {
-const {alt, fib4} = useContext(ApplicationContext);
+export default function LowRiskContent() {
+  const {alt, fib4} = useContext(ApplicationContext);
 
   return (
     <>
-        <TextBasic fontSize={24}>
-          FIB4 = {fib4.toFixed(2)}
+      <TextBasic fontSize={24}>FIB4 = {fib4.toFixed(2)}</TextBasic>
+      {alt >= 40 && (
+        <>
+          <TextBasic fontSize={24} marginVertical={5}>
+            {`ALT \u2265 40 U/L`}
+          </TextBasic>
+          <TextBasic fontSize={14}>
+            Risk of other forms of liver disease
+          </TextBasic>
+        </>
+      )}
+      {alt < 40 && (
+        <TextBasic fontSize={24} marginVertical={5}>
+          {`ALT \u003c 40 U/L`}
         </TextBasic>
-        { 
-          alt >= 40 && (
-            <>
-              <TextBasic 
-                fontSize={24}
-                marginVertical={5}
-              >
-                { `ALT \u2265 40 U/L` }
-              </TextBasic>
-              <TextBasic fontSize={14}>
-                Risk of other forms of liver disease
-              </TextBasic>
-            </>
-          ) 
-        }
-        {
-          alt < 40 && 
-            <TextBasic 
-              fontSize={24}
-              marginVertical={5}
-            >
-              { `ALT \u003c 40 U/L` }
-            </TextBasic>
-        }
+      )}
 
-        <LowRiskDial>
-          <LowRisk />
-        </LowRiskDial>
-        <TextBasic fontSize={ 20 } marginHorizontal={ 50 }>
-          <GreenText>LOW RISK</GreenText> of advanced fibrosis related to NAFLD/NASH
-        </TextBasic>
+      <LowRiskDial>
+        <LowRisk />
+      </LowRiskDial>
+      <TextBasic fontSize={20} marginHorizontal={50} fontColor="#77c83f" fontWeight='bold'>
+        LOW RISK
+      </TextBasic>
     </>
   );
 }
