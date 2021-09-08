6f8cb69bc30891a85a1a035014aec7151096706c
John Serrao <bigbadserrao@gmail.com>
2021-09-07 21:17:41 -0500

    TitleContainer can take a background color now

--- src/components/global/titleContainer.js
+++ src/components/global/titleContainer.js
@@ -9,16 +9,26 @@ import styled from 'styled-components/native'
  */
 
 const Title = styled.View`
+  backgroundColor: ${(props) => props.backgroundColor || 'transparent'};  
   marginBottom: ${(props) => props.marginBottom || '0px'};
   paddingTop: 50px;
   paddingHorizontal: 20px;
   paddingBottom: ${(props) => props.paddingBottom || '0px'};
 `
 
-export default function TitleContainer({children, paddingBottom, marginBottom}) {
+export default function TitleContainer({
+  backgroundColor,
+  children, 
+  paddingBottom, 
+  marginBottom
+}) {
 
   return (
-    <Title paddingBottom={paddingBottom} marginBottom={marginBottom}>
+    <Title 
+      backgroundColor={backgroundColor}
+      paddingBottom={paddingBottom} 
+      marginBottom={marginBottom}
+    >
       {children}
     </Title>
   );
